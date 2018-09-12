6d724961cdd8fe0b4e18726c5d7c5f0ca663a96d
twinBoys <cheng_chang@hotmail.com>
2018-09-11 22:34:34 -0400

    Initial commit

---
+++ src/main/java/com/example/PizzaShop/PizzaShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.PizzaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShopApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/PizzaShop/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.example.PizzaShop.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping(value = "/pizzas")
+    public String getPizzas() {
+        return "Pizzas are good!";
+    }
+
+}
